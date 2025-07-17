e97bb0f21d559b235a0940f4770a62994456ca8f
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-07-17 05:47:33 +0300

    Update click.ps1

--- click.ps1
+++ click.ps1
@@ -48,12 +48,91 @@ function Take-Screenshot {
 Move-Click 541 742
 
 Move-Click 443 748
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 6
 
 #tarayıcılar açıldı edge butona bastı
 Start-Sleep -Seconds 1
 Move-Click 235 619
 Start-Sleep -Seconds 2
 
+
+Move-Click 541 747
+Start-Sleep -Seconds 3    
+
+Move-Click 887 661
+Start-Sleep -Seconds 1
+
+Move-Click 504 677
+Start-Sleep -Seconds 1
+Move-Click 675 675
+Start-Sleep -Seconds 1
+# google okey addpn girişi altta
+Move-Click 450 471
+Start-Sleep -Seconds 1
+
+#edge g
+Move-Click 444 744
+Start-Sleep -Seconds 1
+Move-Click 100 595
+Start-Sleep -Seconds 1
+Move-Click 541 742
+Start-Sleep -Seconds 1
+#edge g
+
+Move-Click 608 128
+Start-Sleep -Seconds 1
+Write-Text "SaveYourTime limit time soon"
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -Seconds 1
+
+#edge g
+Move-Click 444 744
+Start-Sleep -Seconds 1
+Move-Click 100 605
+Start-Sleep -Seconds 1
+Move-Click 541 742
+Start-Sleep -Seconds 1
+#edge g
+
+Move-Click 439 333
+#chrome sayfasına girdik indirme kaldı
+
+#edge g
+Start-Sleep -Seconds 1
+Move-Click 444 744
+Start-Sleep -Seconds 2
+Move-Click 100 610
+Start-Sleep -Seconds 1
+Move-Click 541 742
+Start-Sleep -Seconds 1
+#edge g
+
+Move-Click 925 700
+Start-Sleep -Seconds 1
+
+Move-Click 921 264
+Start-Sleep -Seconds 1
+Move-Click 820 252
+
+Start-Sleep -Seconds 3
+Move-Click 578 254
+
+
+Start-Sleep -Seconds 5
 Take-Screenshot -filename "screenshot1.png"
+# === Başlangıç
+#Write-Text "SaveYourTime limit time soon"
+#[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+
+
+Start-Sleep -Seconds 1
+Move-Click 444 744
+#Start-Sleep -Seconds 2
+#Move-Click 100 600
+#Start-Sleep -Seconds 3
+#Move-Click 100 610
+Start-Sleep -Seconds 2
+
 Take-Screenshot -filename "screenshot2.png"
+
