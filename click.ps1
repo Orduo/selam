a381a457656a453fc7b02347a6297acb11f55ea4
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-07-19 01:48:35 +0300

    Update click.ps1

--- click.ps1
+++ click.ps1
@@ -208,9 +208,11 @@ Start-Sleep -Seconds 1
 Move-Click 543 209
 Start-Sleep -Seconds 1
 [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -Seconds 1
+Take-Screenshot -filename "screenshot1.png"
 Move-Click 543 209
 Start-Sleep -Seconds 8
-Take-Screenshot -filename "screenshot1.png"
+Take-Screenshot -filename "screenshot2.png"
 Start-Sleep -Milliseconds 200
 Move-Click 449 482
 Start-Sleep -Seconds 2
@@ -224,4 +226,4 @@ Start-Sleep -Seconds 1
 
 
 Start-Sleep -Seconds 3
-Take-Screenshot -filename "screenshot2.png"
+#ss
